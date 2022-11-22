451c8d775d7055d4f824ca377f6a823f50d8251f
EvertonSantos774 <71408985+EvertonSantos774@users.noreply.github.com>
2022-11-22 13:25:03 -0300

    Add files via upload

---
+++ sources/org/shaded/apache/http/protocol/RequestTargetHost.java
@@ -0,0 +1,46 @@
+package org.shaded.apache.http.protocol;
+
+import com.google.api.client.http.HttpMethods;
+import java.io.IOException;
+import java.net.InetAddress;
+import org.shaded.apache.http.HttpConnection;
+import org.shaded.apache.http.HttpException;
+import org.shaded.apache.http.HttpHost;
+import org.shaded.apache.http.HttpInetConnection;
+import org.shaded.apache.http.HttpRequest;
+import org.shaded.apache.http.HttpRequestInterceptor;
+import org.shaded.apache.http.HttpVersion;
+import org.shaded.apache.http.ProtocolException;
+import org.shaded.apache.http.ProtocolVersion;
+
+public class RequestTargetHost implements HttpRequestInterceptor {
+    public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
+        if (request == null) {
+            throw new IllegalArgumentException("HTTP request may not be null");
+        } else if (context == null) {
+            throw new IllegalArgumentException("HTTP context may not be null");
+        } else {
+            ProtocolVersion ver = request.getRequestLine().getProtocolVersion();
+            if ((!request.getRequestLine().getMethod().equalsIgnoreCase(HttpMethods.CONNECT) || !ver.lessEquals(HttpVersion.HTTP_1_0)) && !request.containsHeader("Host")) {
+                HttpHost targethost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+                if (targethost == null) {
+                    HttpConnection conn = (HttpConnection) context.getAttribute(ExecutionContext.HTTP_CONNECTION);
+                    if (conn instanceof HttpInetConnection) {
+                        InetAddress address = ((HttpInetConnection) conn).getRemoteAddress();
+                        int port = ((HttpInetConnection) conn).getRemotePort();
+                        if (address != null) {
+                            targethost = new HttpHost(address.getHostName(), port);
+                        }
+                    }
+                    if (targethost == null) {
+                        if (!ver.lessEquals(HttpVersion.HTTP_1_0)) {
+                            throw new ProtocolException("Target host missing");
+                        }
+                        return;
+                    }
+                }
+                request.addHeader("Host", targethost.toHostString());
+            }
+        }
+    }
+}
